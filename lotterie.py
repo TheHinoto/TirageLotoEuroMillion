58af0106dc8247ef3684dc792c0fc398b25d6c88
Zinoto <david@deschanel.fr>
2023-09-25 11:46:14 +0200

    refact

--- lotterie.py
+++ lotterie.py
@@ -1,10 +1,11 @@
 import random
-
+from os import urandom
 
 class TirageEuroMillion:
     def __init__(self) -> None:
-        self.all_existing_loto_number = list(range(1, 50+1))
-        self.all_existing_star_number = list(range(1, 12+1))
+        random.seed(urandom(2048))
+        self.all_existing_loto_number = list(range(1, 50 + 1))
+        self.all_existing_star_number = list(range(1, 12 + 1))
 
         self.numbers: list[int] = random.sample(self.all_existing_loto_number, 5)
         self.stars: list[int] = random.sample(self.all_existing_star_number, 2)
@@ -15,10 +16,12 @@ class TirageEuroMillion:
     def __repr__(self) -> str:
         return f"TirageLoto(Numbers: {self.numbers}, Stars: {self.stars})"
 
+
 class Tirageloto:
     def __init__(self) -> None:
-        self.all_existing_loto_number = list(range(1, 49+1))
-        self.all_existing_star_number = list(range(1, 10+1))
+        random.seed(urandom(2048))
+        self.all_existing_loto_number = list(range(1, 49 + 1))
+        self.all_existing_star_number = list(range(1, 10 + 1))
 
         self.numbers: list[int] = random.sample(self.all_existing_loto_number, 5)
         self.stars: list[int] = random.sample(self.all_existing_star_number, 1)
